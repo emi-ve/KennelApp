da4de31378b4d1c498d2d6f459accfc66385294a
Emily V <emily.venturino@outlook.com>
2021-08-09 19:38:58 -0500

    first commit

---
+++ src/components/Kennel.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { AnimalCard } from "./animal/AnimalCard"
+import { EmployeeCard } from "./employee/EmployeeCard"
+import "./animal/Animal.css"
+import "./employee/Employee.css"
+
+
+
+export const Kennel = () => (
+    <>
+        <h2>Nashville Kennels</h2>
+        <small>Loving care when you're not there.</small>
+
+        <address>
+            <div>Visit Us at the Nashville North Location</div>
+            <div>500 Puppy Way</div>
+        </address>
+
+        <h2>Animals</h2>
+        <article className="animals">
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+        </article>
+
+        <h2>Employees</h2>
+        <article className="employees">
+            <EmployeeCard />
+            <EmployeeCard />
+            <EmployeeCard />
+        </article>
+    </>
+)
